94c4ce82851697f22a5c242b28cd02e4daa00cd3
Salman Akhtar <TheLeopards@protonmail.com>
2020-01-22 00:11:07 +0500

    SHA-3 Basics

--- sha3.py
+++ sha3.py
@@ -1,2 +1,9 @@
+
 #python3 code for sha3 cryptographic algorithm
 import random
+l=6 #value of l = {0, 1, 2, 3, 4, 5, 6}
+b = 25*(2**l) #b = state size (value of b = {25, 50, 100, 200, 400, 800, 1600} )
+#For SHA-3 the value of ‘l’ was 6
+#so rounds turn out to be
+rounds=12 + 2*l # 24 rounds
+print(rounds+' Rounds in SHA-3')
